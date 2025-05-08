0e232a3f63bd86aa0ef4df7527a6fd689a132012
cpassos0127 <cpassos@smartassetcontrol.com.br>
2025-05-08 19:07:34 -0300

    Ajusta porta padrão para 8080 conforme exigido pelo Cloud Run

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,11 @@ app.get('/', (req, res) => {
   res.send('API Smart Asset Control funcionando!');
 });
 
-// AQUI É O PONTO CRÍTICO 👇
+// 🔥 PORTA OBRIGATORIAMENTE EM process.env.PORT
 const PORT = process.env.PORT || 8080;
-
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
 
 
