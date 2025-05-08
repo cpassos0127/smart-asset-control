43b9b8766748f10c2f73e0ab077a204ec53b8975
cpassos0127 <cpassos@smartassetcontrol.com.br>
2025-05-08 18:51:36 -0300

    Corrige PORT para process.env.PORT no Cloud Run

--- backend/index.js
+++ backend/index.js
@@ -7,5 +7,6 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(\Servidor rodando na porta \\);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
+
